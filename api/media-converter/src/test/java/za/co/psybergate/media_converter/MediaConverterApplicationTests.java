6d4f9f2bcc150052fd576079f4a366714244d64b
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-02-11 23:12:26 +0200

    Add Spring boot app for api

---
+++ api/media-converter/src/test/java/za/co/psybergate/media_converter/MediaConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.psybergate.media_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
